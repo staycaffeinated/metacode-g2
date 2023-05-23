c9e3db45b6ac189d876d2d7a05c201684c7b829c
Jon Caulfield <danesub@yahoo.com>
2023-05-23 16:12:04 -0500

    feat: add skeleton build for subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ rootProject.name="metacode-kotlin"
 dependencyResolutionManagement {
   includeBuild("application/mc-annotations")
   includeBuild("application/mc-cli")
-}
+  includeBuild("application/mc-generator")
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-batch")
 
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-boot")
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-shared")
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-webmvc")
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-webflux")
+
+}
 
