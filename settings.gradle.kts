62490a5f26ee227210302c07e94ef515ffee62e5
Jon Caulfield <danesub@yahoo.com>
2023-05-24 09:24:05 -0500

    fix: resolve plugin discovery in sub-sub-components

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ dependencyResolutionManagement {
   includeBuild("application/mc-annotations")
   includeBuild("application/mc-cli")
   includeBuild("application/mc-generator")
-  includeBuild("application/mc-generator-adapter-spring/generator-spring-batch")
 
+  includeBuild("application/mc-generator-adapter-spring/generator-spring-batch")
   includeBuild("application/mc-generator-adapter-spring/generator-spring-boot")
   includeBuild("application/mc-generator-adapter-spring/generator-spring-shared")
   includeBuild("application/mc-generator-adapter-spring/generator-spring-webmvc")
