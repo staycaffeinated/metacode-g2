b5772a57631bd0d22c4af2c93611a51f1ed0b2db
Jon Caulfield <danesub@yahoo.com>
2023-06-01 08:37:58 -0500

    chore: wire in adapter-spring-webflux

--- application/mc-adapter-spring-webflux/build.gradle.kts
+++ application/mc-adapter-spring-webflux/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     id("mmm.coffee.java-conventions")
     id("mmm.coffee.lifecycle")
+    id("io.freefair.lombok") version "8.0.1"
 }
 
 group = "mmm.coffee.metacode"
 
 dependencies {
+    annotationProcessor(libs.lombok)
+    compileOnly(libs.lombok)
     implementation("mmm.coffee.metacode:mc-common")
     implementation("mmm.coffee.metacode:mc-annotations")
     implementation("mmm.coffee.metacode:mc-generator-adapter-spring")
     implementation(libs.freemarker)
     implementation(libs.guice)
     implementation(libs.jacksonYaml)
+    implementation(libs.slf4jApi)
 }
