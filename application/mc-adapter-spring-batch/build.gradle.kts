8fc6b3e7c72589fb1a83ab32d549327135f17b1f
Jon Caulfield <danesub@yahoo.com>
2023-06-01 07:33:21 -0500

    chore: wire in adapter-spring-batch

--- application/mc-adapter-spring-batch/build.gradle.kts
+++ application/mc-adapter-spring-batch/build.gradle.kts
@@ -1,13 +1,16 @@
-
 plugins {
-  id("mmm.coffee.java-conventions")
-  id("mmm.coffee.lifecycle")
+    id("mmm.coffee.java-conventions")
+    id("mmm.coffee.lifecycle")
 }
 
 group = "mmm.coffee.metacode"
 
 dependencies {
-  implementation(libs.freemarker)
-  implementation(libs.guice)
-  implementation(libs.jacksonYaml)
+    implementation("mmm.coffee.metacode:mc-common")
+    implementation("mmm.coffee.metacode:mc-annotations")
+    implementation("mmm.coffee.metacode:mc-generator-adapter-spring")
+
+    implementation(libs.freemarker)
+    implementation(libs.guice)
+    implementation(libs.jacksonYaml)
 }
